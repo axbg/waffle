54ede54c1068f850092f4c91bb6a2382cbd791ed
axbg <bisagalexstefan@gmail.com>
2023-05-27 18:56:08 +0300

    Implemented rotation slow-down on stop and added test fireworks effect

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import CarouselComponent from './components/CarouselComponent.vue';
 import GalleryComponent from './components/GalleryComponent.vue';
-
 </script>
 
 <template>
-  <CarouselComponent/>
+  <CarouselComponent defaultTimeoutUpperLimit="600" defaultTimeoutLowerLimit="100" numberOfImages="25"/>
   <!-- <GalleryComponent/> -->
 </template>
 
 <style scoped>
-
 </style>
--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,32 +1,47 @@
 <script setup>
 import { computed } from '@vue/reactivity';
 import { reactive } from 'vue';
+import { Fireworks } from '@fireworks-js/vue';
 
 const image_base = "/unsamples/image-";
 const ext = ".jpg"
-const state = reactive({image: 1, interval: null});
 
-let dynamicTimeout = 1000;
+const speedUp = (x) => x.timeout > props.defaultTimeoutLowerLimit ? x.timeout * 0.9 : x.timeout;
+const slowDown = (x) => x.timeout <= props.defaultTimeoutUpperLimit ? x.timeout * 1.1 : x.timeout;
+
+const props = defineProps(['defaultTimeoutUpperLimit', 'defaultTimeoutLowerLimit', 'numberOfImages']);
+
+const state = reactive({
+    image: 1, 
+    interval: null, 
+    timeout: props.defaultTimeoutUpperLimit, 
+    currentFormula: speedUp,
+    showFireworks: true
+});
 
 const computedName = computed(() => {
     return image_base + state.image + ext;
 });
 
 const runImages = () => {
-    state.image = state.image >= 25 ? 1 : state.image + 1;
-
-    dynamicTimeout = dynamicTimeout > 100 ? dynamicTimeout * 0.9 : dynamicTimeout;
+    state.image = state.image >= props.numberOfImages ? 1 : state.image + 1;
+    state.timeout = state.currentFormula(state);
 
     clearInterval(state.interval);
-    state.interval = setInterval(runImages, dynamicTimeout);
+
+    if(state.timeout <= props.defaultTimeoutUpperLimit) {
+        state.interval = setInterval(runImages, state.timeout);
+    } else {
+        state.showFireworks = true;
+    }
 }
 
 const startInterval = () => {
-    state.interval = setInterval(runImages, dynamicTimeout);
+    state.interval = setInterval(runImages, state.timeout);
 };
 
 const stopInterval = () => {
-    clearInterval(state.interval);
+    state.currentFormula = slowDown;
 }
 </script>
 
@@ -34,12 +49,32 @@ const stopInterval = () => {
     <div class="image-container">
         <img :src="computedName" width="1000" height="400">
     </div>
-    <button @click="startInterval">Start</button>
-    <button @click="stopInterval">Stop</button>
+    <div class="control-container">
+        <button @click="startInterval">Start</button>
+        <button @click="stopInterval">Stop</button>
+    </div>
+    <div class="fireworks-container">
+        <Fireworks v-if="state.showFireworks"/>
+    </div>
 </template>
 
 <style>
 .image-container {
-    display: block;
+    text-align: center;
+}
+.control-container {
+    margin: 0 auto;
+    width: 20%;
+    display: flex;
+    justify-content: space-between;
+}
+.control-container button {
+    width: 100px;
+}
+.fireworks-container {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 80%;
 }
 </style>
