88ff91b4be77422f08f9b7fe4e8e1fc81cb029cd
axbg <bisagalexstefan@gmail.com>
2023-05-28 17:29:10 +0300

    Integrated Control icon and modal

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { ref } from 'vue';
+import { Modal } from 'usemodal-vue3';
+
+const visibleModal = ref(false);
+
+const toggleControlModal = () => {
+    visibleModal.value = !visibleModal.value;
+}
 </script>
 
 <template>
     <div class="header-container">
         <img src="/logo-placeholder.png" class="logo">
         <img src="/logo-placeholder.png" class="logo">
+        <div class="control-button" @click="toggleControlModal">
+            <font-awesome-icon icon="gear" size="xl"/>            
+        </div>
     </div>
+    <Modal v-model:visible="visibleModal" title="Control Options">
+        <div>fancy Control modal</div>
+    </Modal>
 </template>
 
 <style scoped>
@@ -17,5 +31,12 @@
 .logo {
     width: 30vw;
     height: 10vh;
+}
+.control-button {
+    position: fixed;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+    padding: 30px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faGear);
+
+createApp(App)
+.component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app')
